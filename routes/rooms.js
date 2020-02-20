30d40f5228b4cbac598b531fe4144f9ce06fafb6
Jayson Hunter <jayson.hunter@pulselive.com>
2020-02-20 20:42:03 +0000

    Add 404 controller and page Update controllers file name and internal variable names Updated reference  to the /public folder

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const room_controller = require('../controllers/roomController.js');
+const roomController = require('../controllers/room');
 
 // GET request to display list of all rooms
-router.get('/', room_controller.room_list);
+router.get('/', roomController.room_list);
 
 // GET request for one Room.
-router.get('/:id', room_controller.room_detail);
+router.get('/:id', roomController.room_detail);
 
 module.exports = router;
