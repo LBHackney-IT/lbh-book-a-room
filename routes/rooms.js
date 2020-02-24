bf1ffde7daa8c1a3fcbda7925ccc09ef4235001b
Jayson Hunter <jayson.hunter@pulselive.com>
2020-02-24 10:20:17 +0000

    Update room routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const roomController = require('../controllers/room');
 
 // GET request to display list of all rooms
-router.get('/', roomController.room_list);
+router.get('/', roomController.room_list_get);
 
 // GET request for one Room.
-router.get('/:id', roomController.room_detail);
+router.get('/:id', roomController.room_detail_get);
 
 module.exports = router;
