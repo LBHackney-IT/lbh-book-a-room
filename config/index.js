ec6a27b12e02ed77043a3efc02c8733db673b063
Jayson Hunter <github@jaysonhunter.com>
2020-03-15 14:49:06 +0000

    Add a protocal config variable and add it to the payment return URL (#40)

--- config/index.js
+++ config/index.js
@@ -7,6 +7,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
     host: process.env.HOST || "localhost",
     port: parseInt(process.env.PORT, 10) || 5000,
+    protocol: process.env.protocol,
     sessionSecret: process.env.SESSION_SECRET,
 
     onsched: {
